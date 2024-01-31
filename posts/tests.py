2c5721b66406878ba42c737bb879a0bf61c068a6
Jordyn Kuhn <jkuhn2472@mail.kvcc.edu>
2024-01-30 20:00:38 -0500

    Set up home page with posts displayed, set up test to test contents of posts.

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Post
+
+class PostTests(TestCase):
+    """Post Tests"""
+
+    @classmethod
+    def setUpTestData(cls):
+        """Set up the data needed for each test"""
+        cls.post = Post.objects.create(text = "This is a test!")
+
+    def test_model_content(self):
+        """Test the model content"""
+        self.assertEqual(self.post.text, "This is a test!")
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Post
+
+class HomePageView(ListView):
+    """Home Page View"""
+
+    model = Post
+    template_name = "home.html"
