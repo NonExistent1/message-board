6371b263b26f5e5f805b21ff44467b06c40e8eae
Jordyn <jkuhn2472@mail.kvcc.edu>
2024-01-30 21:30:27 -0500

    Add requirements.txt. =Add more testing for the webpage.

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Post
 
@@ -13,3 +14,15 @@ class PostTests(TestCase):
     def test_model_content(self):
         """Test the model content"""
         self.assertEqual(self.post.text, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        """Test url exists at correct location"""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        """Test the home page"""
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home.html")
+        self.assertContains(response, "This is a test!")
